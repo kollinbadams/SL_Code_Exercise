fc66a9a319150f7dfc708dc0a6ea91585d5e3b00
kollinbadams <40585343+kollinbadams@users.noreply.github.com>
2018-10-29 22:40:51 -0400

    Updated people.rb

--- app/models/people.rb
+++ app/models/people.rb
@@ -1,1 +1,13 @@
-people.rb file placeholder
+class People < ApplicationRecord
+
+	API_BASE_URL = "https://api.salesloft.com/v2"
+	#Tell the API to look for people, and per page function
+	SUB_PATH = "/people.json?per_page="
+	#Test Page Size Function
+	PAGE_SIZE = "1"
+	#Create full URL string
+	URL = "#{API_BASE_URL}#{SUB_PATH}#{PAGE_SIZE}"
+	#Include specified Headers, and reference API KEY
+	HEADERS = {"Authorization" => "Bearer #{ENV.fetch("SALESLOFT_API_KEY")}"}
+
+	#TODO Add get method, and initialize people object
