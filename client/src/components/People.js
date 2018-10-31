320e547b42e050d9c66ede9fecdb66a3d806bb39
kollinbadams <40585343+kollinbadams@users.noreply.github.com>
2018-10-30 22:41:57 -0400

    Update People.js

--- client/src/components/People.js
+++ client/src/components/People.js
@@ -1,1 +1,42 @@
-//People.js file placeholder
+import React, {Component} from 'react'
+import styled from 'styled-components'
+import {connect} from 'react-redux'
+
+import { getPeople } from '../actions/AsyncActions'
+import PeopleTable from './PeopleTable'
+
+class People extends Component {
+	constructor() {
+		super()
+		this.state = {
+			people: []
+		}
+	}
+	//Mount the response to the props and state to be passed through the components
+	componentDidMount(){
+		this.props.getPeople().
+		then( r => this.setState({people: r.people}) )
+	}
+	//Render the Table with data from API
+	//Can Swap div for a stylized container after testing functionality
+	render() {
+		const { people } = this.state
+
+		return (
+			<div>
+				<h3>SalesLoft: List of People</h3>
+				<div>
+					<PeopleTable people={people}/>
+				</div>
+			</div>
+		)
+	}
+}
+
+//Map the state and dispatch to Action
+const mapStateToProps = (dispatch) => ({
+    getPeople: () => dispatch(getPeople())
+})
+
+//Connect the mapState to People
+export default connect(null, mapStateToProps)(People)
