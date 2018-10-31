8b9cfe523f2c11ddaac542bc5741302820d3d641
kollinbadams <40585343+kollinbadams@users.noreply.github.com>
2018-10-30 22:32:42 -0400

    Updated People Model
    Added fetch_data method to grab the api data using httparty. Added test, object implementation.

--- app/models/people.rb
+++ app/models/people.rb
@@ -1,5 +1,6 @@
 class People < ApplicationRecord
-
+	include HTTParty
+	
 	API_BASE_URL = "https://api.salesloft.com/v2"
 	#Tell the API to look for people, and per page function
 	SUB_PATH = "/people.json?per_page="
@@ -10,4 +11,26 @@ class People < ApplicationRecord
 	#Include specified Headers, and reference API KEY
 	HEADERS = {"Authorization" => "Bearer #{ENV.fetch("SALESLOFT_API_KEY")}"}
 
-	#TODO Add get method, and initialize people object
+	#Fetch data using HTTParty with URL and HEADERS
+	def fetch_data
+		api_response = HTTParty.get(URL, headers: HEADERS)
+		data = api_response["data"]
+		return data
+	end
+
+	#Create new object
+	people = People.new
+
+	#test fetch_data results
+	puts people.fetch_data
+
+	#implementation:
+	people.fetch_data.each do |item|
+		People.create do |p|
+	#		Attempting to get full name from API instead of first and last, then combining them
+			p.display_name = item["display_name"]
+			p.email_address = item["email_address"]
+			p.title = item["title"]
+		end
+	end
+end
