81f90641735a7a094e529d80631122494c98bbb7
kollinbadams <40585343+kollinbadams@users.noreply.github.com>
2018-10-30 22:39:28 -0400

    Updated PeopleTable.js

--- client/src/components/PeopleTable.js
+++ client/src/components/PeopleTable.js
@@ -1,1 +1,25 @@
-//PeopleTable.js file placeholder
+import React from 'react'
+import styled from 'styled-components'
+
+const PeopleTable = (props) => { 
+	return (
+		<div>
+			<table>
+				<tr>
+					<th>Name</th>
+					<th>Title</th>
+					<th>Email Address</th>
+				</tr>
+				{props.people.map(p =>
+				<tr>
+					<td>{p.display_name}</td>
+					<td>{p.title}</td>
+					<td>{p.email_address}</td>
+				</tr>
+				)}
+			</table>
+		</div>
+		)
+}
+
+	export default PeopleTable
