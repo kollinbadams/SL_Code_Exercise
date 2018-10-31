18b33b3599fe2cd493f87fc9d7e4cb52945489f6
kollinbadams <40585343+kollinbadams@users.noreply.github.com>
2018-10-30 22:34:34 -0400

    Updated people_controller.rb
    Implemented people_controller

--- app/controllers/api/people_controller.rb
+++ app/controllers/api/people_controller.rb
@@ -1,1 +1,10 @@
-people_controller.rb file placeholder
+class Api::PeopleController < Api::BaseController
+	respond_to :json
+	
+	#Set response to all data within people object, and render the json data
+	def index
+		@people = People.all
+		response = @people
+		render json: response
+	end
+end
