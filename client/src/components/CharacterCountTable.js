1f47c4057836d69ec7c37d38291a76139b4df9b3
kollinbadams <40585343+kollinbadams@users.noreply.github.com>
2018-11-02 21:08:30 -0400

    Level 2 Character Count Table

---
+++ client/src/components/CharacterCountTable.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+display: flex;
+justify-content:center;
+align-items: center;
+table {
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+    width: 30%;
+}
+td, th {
+    border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;
+}
+tr:nth-child(even) {
+    background-color: #e9e9e9;
+}
+`;
+
+const CharacterCountTable = (props) => {
+	return(
+		<Container>
+			<table>
+				<tbody>
+					<tr>
+						<th>Character</th>
+						<th>Count</th>
+					</tr>
+					{props.count.map(c =>
+					<tr key={c.char}>
+						<td>{c.char}</td>
+						<td>{c.count}</td>
+					</tr>
+					)}
+				</tbody>
+			</table>
+		</Container>
+	)
+}
+
+export default CharacterCountTable
