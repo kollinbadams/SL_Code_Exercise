55b5720f3a5fea8acf5c389fde888351ede1a0da
kollinbadams <40585343+kollinbadams@users.noreply.github.com>
2018-11-02 21:29:15 -0400

    Commented level 2 logic

--- client/src/utils.js
+++ client/src/utils.js
@@ -2,11 +2,12 @@ export const merge = (...obj) => Object.assign({}, ...obj);
 
 //set up keys and set values to 0 initially
 //loop through range of character codes covering all codes
+//character range from 1-9, a-z, a few special characters
 const dict = []
 for (var i= 48; i<91; i++){
 	dict.push({char: String.fromCharCode(i), count:0});
 };
-console.log(dict)
+//console.log(dict)
 
 export function countCharacters(input){
 //logic for counting character and setting the Key:value pair
@@ -19,6 +20,8 @@ export function countCharacters(input){
 			}
 		}
 	}
-	console.log(dict)
+	//testing result
+	//console.log(dict)
+	//return filtered dictionary, excluding character counts of 0, sorting desc, handling for ties
 	return dict.filter(c => c.count !== 0).sort((a,b) => b.count - a.count)
 }
