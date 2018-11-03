4250a64ac6380dcaa6d495549aa592190a9d1822
kollinbadams <40585343+kollinbadams@users.noreply.github.com>
2018-11-02 21:26:25 -0400

    Updated comments after level 2

--- client/src/components/People.js
+++ client/src/components/People.js
@@ -7,7 +7,7 @@ import { countCharacters } from '../utils'
 import PeopleTable from './PeopleTable'
 import CharacterCountTable from './CharacterCountTable'
 
-
+//Button styling
 const Button = styled.button`
   background: #4a90e2;
   padding: 1em;
@@ -17,9 +17,11 @@ const Button = styled.button`
   font-size:1em;
 `;
 
+
 class People extends Component {
 	constructor() {
 		super()
+		//People, char array, and click states
 		this.state = {
 			people: [],
 			chars: [],
@@ -31,22 +33,27 @@ class People extends Component {
 		this.props.getPeople().
 		//then( (r) => console.log(r))
 		then( (r) => this.setState({people: r.people}, () => {
+		//prevent character count state from loading more than once on page reload, map it to send to characterCount method
 		this.state.people.map(c => this.characterCounter(c.email_address))
 		}))
 	}
 
+	//pass the array of characters to the util function for handling the counting logic
 	characterCounter(allChars){
 		this.setState({ chars: countCharacters(allChars.toUpperCase())})
 	}
 
+	//custom click handler, adjusting the state of the click
 	handleClick(){
 		this.setState({isClicked: !this.state.isClicked})
 	}
 
 	//Render the Table with data from API
-	//Can Swap div for a stylized container after testing functionality
 	render() {
+		//link the states
 		const { people, chars, isClicked } = this.state
+		
+		//button title setup
 		const viewPrompt = "View Character Count"
 		const closePrompt = "Close Character Count"
 		const characterCountButtonTitle = isClicked ? closePrompt : viewPrompt
