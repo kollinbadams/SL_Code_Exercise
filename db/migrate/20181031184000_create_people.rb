0caaabe5169066787aab09f0e484a6f06099af69
kollinbadams <40585343+kollinbadams@users.noreply.github.com>
2018-11-02 21:16:52 -0400

    Updated migrate script - includes pid column

--- db/migrate/20181031184000_create_people.rb
+++ db/migrate/20181031184000_create_people.rb
@@ -1,6 +1,7 @@
 class CreatePeople < ActiveRecord::Migration[5.1]
   def change
     create_table :people do |t|
+      t.bigint :pid
       t.string :display_name
       t.string :email_address
       t.string :title
